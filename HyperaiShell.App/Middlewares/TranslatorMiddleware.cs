1d4dbef97b75ee4e31f211ce3fe43d717be492aa
Chien Zhang <2419328026@qq.com>
2021-04-17 18:14:25 +0800

    TranslatorMiddleware 加了个try

--- HyperaiShell.App/Middlewares/TranslatorMiddleware.cs
+++ HyperaiShell.App/Middlewares/TranslatorMiddleware.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Hyperai.Events;
 using Hyperai.Messages;
 using Hyperai.Messages.ConcreteModels;
@@ -22,7 +23,19 @@ namespace HyperaiShell.App.Middlewares
             {
                 var text = string.Join(string.Empty, msgEvent.Message.OfType<Plain>().Select(x => x.Text));
                 if (text.Length > 8 && (text.StartsWith("```\r") || text.StartsWith("```\n")) &&
-                    (text.EndsWith("\r```") || text.EndsWith("\n```"))) msgEvent.Message = _parser.Parse(text[4..^4]);
+                    (text.EndsWith("\r```") || text.EndsWith("\n```")))
+                {
+                    try
+                    {
+                        var msg = _parser.Parse(text[5..^4]);
+                        msgEvent.Message = msg;
+                    }
+                    catch (Exception e)
+                    {
+                        // not proper hyper code
+                    }
+
+                }
             }
 
             return true;
